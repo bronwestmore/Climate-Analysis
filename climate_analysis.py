7a44acde2c6304999630442682a19f7bd3a1854b
Bronwyn Westmore <bjw1g20@soton.ac.uk>
2022-10-27 10:43:57 +0100

    Add rainfall processing placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,5 +1,4 @@
 """Climate Analysis Tools """
-
 import sys
 import temp_conversion
 import signal
@@ -27,3 +26,6 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+# TODO(smangham) : Add call to process rainfall
+
